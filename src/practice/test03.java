dace2c51c09ed8f5293bdf9fa01f3db63961a241
huni93 <pkh0699@naver.com>
2023-11-30 08:31:54 +0900

    init

---
+++ src/practice/test03.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class test03 {
+
+	public static void main(String[] args) {
+		String s1 = new String("java");
+		String s2 = "java"
+	    if (s1.equals(s2)) System.out.println("1");
+		if(s1 == s2) System.out.println("2");
+	}
+
+}
